fd07e445b145e0b9538bf89deaec662e9fbb4bdb
SIDDHARTH SRIDHAR <84910325+SRIDHAR2021SIDDHARTH@users.noreply.github.com>
2021-07-15 10:28:59 +0530

    Update aria.py

--- userbot/modules/aria.py
+++ userbot/modules/aria.py
@@ -1,6 +1,6 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+# Copyright (C) 2021 The Mahadev Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# Licensed under the MahaDev Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 
 import math
